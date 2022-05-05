d704db9aab3c4ce40b51ed506749252043c0bd87
cbg <cbg@qq.com>
2022-05-05 13:50:35 +0800

    first master commit

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello test");
         System.out.println("hello git");
-        System.out.println("hello git hot-fix");
+        System.out.println("hello git hot-master");
 
     }
 }
