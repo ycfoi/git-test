48cb3c7785e138a3b100d23e26bdc3ac3c37ca72
cbg <cbg@qq.com>
2022-05-05 13:26:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @author shkstart
+ * @create 2022-05-05 13:23
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
